ee8dbf886d361b09c8f2880e80704e1dc8c7cb2c
Jesse Wei <jessew13@email.unc.edu>
2021-01-26 20:54:47 -0500

    Worked on a skeleton for 210JavaPractice.

--- 210JavaPractice/src/Main.java
+++ 210JavaPractice/src/Main.java
@@ -1,6 +1,35 @@
+// Prompt for this can be found here: https://www.cs.unc.edu/~stotts/COMP210-s21/Java/ufo.html
+
 public class Main {
+
+    // remember to go big picture first, don't get bogged down by details
 
     public static void main(String[] args){
 
+        // call read_args to get data
+
+        // call
+
     }
+
+    // a function that takes in the data's file path and returns a String matrix of the data
+    public static String[][] read_args(String path){
+        String[][] x = new String[0][0];
+        return x;
+    }
+
+    // a function that converts a column within a String matrix to an array of numerical data, can be int or float
+    public static int[] convertToIntArray(String[][] data){
+        int[] x = new int[0];
+        return x;
+    }
+
+    // might not need this one, we'll see
+    public static double[] convertToFloatArray(String[][] data){
+        double[] x = new double[0];
+        return x;
+    }
+
+    // a function that finds the row number of the max/min (variable) element of a numerical array
+    // could actually write two functions with the same name but with different return/param types here
 }
