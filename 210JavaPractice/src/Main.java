b6f8289b6be4564477f602a5ec013b6234f6cefe
Jesse Wei <jessew13@email.unc.edu>
2021-01-27 13:26:45 -0500

    Did some work on 210JavaPractice. readTxt does not function.

--- 210JavaPractice/src/Main.java
+++ 210JavaPractice/src/Main.java
@@ -1,35 +1,84 @@
 // Prompt for this can be found here: https://www.cs.unc.edu/~stotts/COMP210-s21/Java/ufo.html
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    final static int COLUMN_OF_DURATIONS = 3;
+    final static int NUM_COLUMNS = 6;
+    final static String PATH = "C:\\Users\\jesse\\IdeaProjects\\learning\\210JavaPractice\\data\\mini.txt";
 
     // remember to go big picture first, don't get bogged down by details
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
-        // call read_args to get data
+        // call readTxt to get data
+        String[][] data = readTxt(PATH);
 
-        // call
+        printArray(data);
+
+        // call convertToIntArray(data, COLUMN_OF_DURATIONS)
+
+        // call maxMinArray to get the row indices of the max and min durations
+
+        // print out info for those row indices from matrix
+
 
     }
 
     // a function that takes in the data's file path and returns a String matrix of the data
-    public static String[][] read_args(String path){
-        String[][] x = new String[0][0];
-        return x;
+    public static String[][] readTxt(String path) throws Exception{
+        File file = new File(PATH);
+        String line;
+        String delimiter = " ";
+        int i = 0;
+        int j;
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        int numRows = Integer.parseInt(br.readLine());
+        String[][] data = new String[numRows][];
+
+        while ((line = br.readLine()) != null) {
+            j = 0;
+            StringTokenizer stObj = new StringTokenizer(line, delimiter);
+            while (stObj.hasMoreElements()){
+                data[i][j] = (String)stObj.nextElement();
+                j++;
+            }
+            i++;
+
+        }
+        return data;
     }
 
     // a function that converts a column within a String matrix to an array of numerical data, can be int or float
-    public static int[] convertToIntArray(String[][] data){
+    public static int[] convertToIntArray(String[][] data, int j){
         int[] x = new int[0];
         return x;
     }
 
     // might not need this one, we'll see
-    public static double[] convertToFloatArray(String[][] data){
+    public static double[] convertToFloatArray(String[][] data, int j){
         double[] x = new double[0];
         return x;
     }
 
-    // a function that finds the row number of the max/min (variable) element of a numerical array
-    // could actually write two functions with the same name but with different return/param types here
+    // a function that finds the indices of the max and min elements of a numerical array, returns it
+    // could actually write two functions with the same name but with different PARAM types here (not return types)
+
+    private static int[] maxMinArray(int[] data){
+        int[] x = new int[0];
+        return x;
+    }
+
+    private static void printArray(String[][] data){
+        for (int i = 0; i < data.length; i++){
+            for (int j = 0; j < data[i].length; j++){
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
