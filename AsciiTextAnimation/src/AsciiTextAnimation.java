43e074a57e69f6197e64ab73476d5594a8147002
Jesse Wei <jessew13@email.unc.edu>
2021-02-02 13:53:31 -0500

    Skeleton of ATA done, gonna test it a little now.

--- AsciiTextAnimation/src/AsciiTextAnimation.java
+++ AsciiTextAnimation/src/AsciiTextAnimation.java
@@ -1,2 +1,78 @@
+import java.util.*;
+
+// Thread.sleep(milliseconds);
+
 public class AsciiTextAnimation {
+
+    static int column;
+    final static int VERTICAL_LENGTH = 5;
+    final static int DELAY = 100;  // in milliseconds
+    final static int NUM_SPACES = 2;
+
+    public static void main(String[] args){
+        // Create new scanner for getting input as to what needs to be encoded in ASCII
+        Scanner input = new Scanner(System.in);
+
+        // Ask for input
+        System.out.println("What would you like encoded in ASCII?");
+        String original = input.next();
+
+        // Determine number of chars/columns needed (columns = 6 * numChars - 2)
+        int numChars = original.length();
+
+        // Create matrix, TODO initialize with all spaces
+        String[][] matrix = new String[VERTICAL_LENGTH][(4 + NUM_SPACES) * numChars - NUM_SPACES];  // 6, representing horizontal width and numSpaces, could be a constant, or make it (4+numSpaces) * numChars - numSpaces
+        fillMatrixWithSpaces(matrix);
+
+        // Iterate through original
+        for (int c = 0; c < original.length(); c++){
+            char ch = original.charAt(c);
+        }
+    }
+
+    private static void drawChar(char ch, String[][] matrix, int numChar){  // TODO remember to 0-index numChar
+        column = 6 * numChar;
+        switch (ch){
+            case 'A': ;
+            case 'B': ;
+            case 'C': ;
+            case 'D': ;
+            case 'H':
+                for (int i = 0; i < 5; i++){
+                    matrix[i][column] = "|";
+                    matrix[i][column + 3] = "|";
+                    pause();
+            }
+                matrix[2][column + 1] = "-";
+                pause();
+                matrix[2][column + 2] = "-";
+                pause();
+            case 'Y': ;
+        }
+    }
+
+    private static void fillMatrixWithSpaces(String[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                matrix[i][j] = " ";
+            }
+        }
+    }
+
+    private static void pause(){
+        try{
+            Thread.sleep(DELAY);
+        } catch (InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void displayMatrix(String[][] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            System.out.println();  // TODO can make this not happen for i = 0 but for all other i
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+        }
+    }
 }
