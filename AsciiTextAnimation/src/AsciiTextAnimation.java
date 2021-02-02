fb66e3cc438c154be83877fd4524ed6013c820fe
Jesse Wei <jessew13@email.unc.edu>
2021-02-02 14:14:33 -0500

    Glitchy.

--- AsciiTextAnimation/src/AsciiTextAnimation.java
+++ AsciiTextAnimation/src/AsciiTextAnimation.java
@@ -6,6 +6,7 @@ public class AsciiTextAnimation {
 
     static int column;
     final static int VERTICAL_LENGTH = 5;
+    final static int HORIZONTAL_WIDTH = 4;
     final static int DELAY = 100;  // in milliseconds
     final static int NUM_SPACES = 2;
 
@@ -21,12 +22,13 @@ public class AsciiTextAnimation {
         int numChars = original.length();
 
         // Create matrix, TODO initialize with all spaces
-        String[][] matrix = new String[VERTICAL_LENGTH][(4 + NUM_SPACES) * numChars - NUM_SPACES];  // 6, representing horizontal width and numSpaces, could be a constant, or make it (4+numSpaces) * numChars - numSpaces
+        String[][] matrix = new String[VERTICAL_LENGTH][(HORIZONTAL_WIDTH + NUM_SPACES) * numChars - NUM_SPACES];  // 6, representing horizontal width and numSpaces, could be a constant, or make it (4+numSpaces) * numChars - numSpaces
         fillMatrixWithSpaces(matrix);
 
         // Iterate through original
         for (int c = 0; c < original.length(); c++){
             char ch = original.charAt(c);
+            drawChar(ch, matrix, c);
         }
     }
 
@@ -34,18 +36,41 @@ public class AsciiTextAnimation {
         column = 6 * numChar;
         switch (ch){
             case 'A': ;
-            case 'B': ;
+            case 'B':
+                for (int i = 0; i < VERTICAL_LENGTH; i++){
+                    matrix[i][column] = "|";
+                    displayMatrix(matrix);
+                    pause();
+                }
+                for (int i = 1; i < 3; i++){
+                    matrix[0][column + i] = "-";
+                    matrix[2][column + i] = "-";
+                    matrix[4][column + i] = "-";
+                    displayMatrix(matrix);
+                    pause();
+                }
+
+                matrix[0][column + 3] = "\\";
+                matrix[3][column + 3] = "\\";
+                matrix[1][column + 3] = "/";
+                matrix[4][column + 3] = "/";
+                displayMatrix(matrix);
+                pause();
+
             case 'C': ;
             case 'D': ;
             case 'H':
-                for (int i = 0; i < 5; i++){
+                for (int i = 0; i < VERTICAL_LENGTH; i++){
                     matrix[i][column] = "|";
                     matrix[i][column + 3] = "|";
+                    displayMatrix(matrix);
                     pause();
             }
                 matrix[2][column + 1] = "-";
+                displayMatrix(matrix);
                 pause();
                 matrix[2][column + 2] = "-";
+                displayMatrix(matrix);
                 pause();
             case 'Y': ;
         }
